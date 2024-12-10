1c810ec46d7e307d01e00932b2cb26a9bcf41679
Oleksii Kovalenko <oleksii.kovalenko31@gmail.com>
2024-12-10 22:18:11 +0100

    Добавлено .gitignor. Зроблено друге завдання

--- HomeWork#10Kovalenko/Program.cs
+++ HomeWork#10Kovalenko/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+
+//  Ви отримали рядок, який містить декілька слів, розділених комами.
+//  Ваша мета - видалити всі пробіли з цього рядка та вивести результат.
 Console.Write("Введіть вираз - ");
 string? str = Console.ReadLine();
 
@@ -14,6 +19,26 @@ foreach (char item in str)
     }
 
 }
+
+//  Ви отримали рядок, який містить ім’я та прізвище користувача.
+//  Ваша мета - перевірити, чи починається прізвище на ту ж літеру, що й ім’я. Якщо так,
+//  вивести повідомлення “Прізвище починається на ту ж літеру, що і ім’я”,
+//  інакше - “Прізвище не починається на ту ж літеру, що і ім’я”.
+
+
+Console.WriteLine("\n");
+Console.Write("Введіть ім'я та прізвище - ");
+string? FamalyName = Console.ReadLine();
+
+if (FamalyName.Substring(0, 1) == FamalyName.Substring(FamalyName.IndexOf(' ') + 1, 1))
+{
+    Console.WriteLine($"Прізвище починається на ту ж літеру, що і ім’я на літеру \"{FamalyName.Substring(0, 1)}\"!");
+}
+else
+{
+    Console.WriteLine($"Прізвище не починається на ту ж літеру, що і ім’я!");
+}
+
 
 
 

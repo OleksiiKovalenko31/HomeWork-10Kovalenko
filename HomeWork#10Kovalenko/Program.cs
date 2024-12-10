54d040fb0876bbc7cf5eb67148d43e9bddad50a9
Oleksii Kovalenko <oleksii.kovalenko31@gmail.com>
2024-12-10 21:23:10 +0100

    Добавьте файлы проекта.

---
+++ HomeWork#10Kovalenko/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введіть вираз - ");
+string? str = Console.ReadLine();
+
+
+foreach (char item in str)
+{
+
+    if (item != ' ')
+    {
+        Console.Write(item);
+    }
+
+}
+
+
+
+
